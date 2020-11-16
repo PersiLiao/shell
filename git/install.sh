717265612286c80f682ae0af403e6dd64a6ad71d
PersiLiao <xiangchu.liao@gmail.com>
2020-11-17 04:57:50 +0800

    fixed git install

--- git/install.sh
+++ git/install.sh
@@ -8,12 +8,12 @@ pushd ${workDirectory} > /dev/null
 workDirectory=$(pwd)
 
 sudo yum install -y curl vim wget curl-devel gcc zlib-devel openssl-devel perl cpio expat-devel gettext-devel openssl zlib curl autoconf tk curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker package gettext-devel asciidoc xmlto
-wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.28.0.tar.gz
+wget -O ~/git-2.28.0.tar.gz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.28.0.tar.gz
 tar -zxvf git-2.28.0.tar.gz
-cd ./git-2.28.0
+cd ~/git-2.28.0
 ./configure
 make -j 4
-make install
+sudo make install
 
 
 
