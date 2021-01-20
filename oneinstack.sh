e0865f1d887046e729838b9df9b2f15f82e431c2
PersiLiao <xiangchu.liao@gmail.com>
2021-01-20 09:00:18 +0800

    update

--- oneinstack.sh
+++ oneinstack.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget -c http://mirrors.linuxeye.com/oneinstack-full.tar.gz && tar xzf oneinstack-full.tar.gz && ./oneinstack/install.sh --nginx_option 1 --php_option 9 --php_extensions imagick,fileinfo,redis --db_option 2 --dbinstallmethod 1 --dbrootpwd oneinstack --redis
+wget -O ~/oneinstack-full.tar.gz -c http://mirrors.linuxeye.com/oneinstack-full.tar.gz && cd ~ && tar xzf oneinstack-full.tar.gz && ./oneinstack/install.sh --nginx_option 1 --php_option 9 --php_extensions imagick,fileinfo,redis --db_option 2 --dbinstallmethod 1 --dbrootpwd oneinstack --redis
