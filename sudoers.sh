c23a5ddc2db0014dcfd00f5ad5970ad0bdcd1261
PersiLiao <xiangchu.liao@gmail.com>
2021-06-02 16:09:12 +0800

    Optimize code

--- sudoers.sh
+++ sudoers.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo 'persi ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+echo "${USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+if [ $? == 0 ]; then
+	echo -e "${SLF}${CSUCCESS}The current user is successfully added to sudoers. ${CEND}${SLF}"
+else
+	echo -e "${SLF}${CFAILURE}Failed to add current user to sudoers !${CEND}${SLF}"
+	exit 1
+fi
--- wp.sh
+++ wp.sh
@@ -2,4 +2,11 @@
 
 curl -O ~/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x ~/wp
-sudo cp ~/wp /usr/local/bin/wp
+sudo mv ~/wp /usr/local/bin/wp
+
+if [ $? == 0 ]; then
+	echo -e "${SLF}${CSUCCESS}wp cli installed successfully. ${CEND}${SLF}"
+else
+	echo -e "${SLF}${CFAILURE}wp cli installed unsuccessfully !${CEND}${SLF}"
+	exit 1
+fi
